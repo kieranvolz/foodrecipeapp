267ab486e7eaf7fa6ed1269067cb583a15c25a2d
Kieran Volz <kieran.volz@gmail.com>
2022-10-26 07:44:55 -0400

    got it to run locally
    now go to the gym to celebrate!!!

--- myfoodapp/src/App.js
+++ myfoodapp/src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+// import "./App.css";
+import Axios from "https://cdn.skypack.dev/axios@0.27.2";  
+import { v4 as uuidv4 } from "https://cdn.skypack.dev/uuid@8.3.2"; 
+//unique id generator from not package
+//import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+
+
+// import Recipe from "./components/Recipe";
+// import Alert from "./components/Alert";
+
+  
+// import React from "https://cdn.skypack.dev/react@17.0.1";
+
+const Alert = ({ alert }) => {
+  return (
+    <div className="alert">
+      <h3>{alert}</h3>
+    </div>
+  );
+};
+
+const Recipe = ({ recipe }) => {
+  const [show, setShow] = useState(false);
+  const { label, image, url, ingredients } = recipe.recipe;
+
+  return (
+    <div className="recipe">
+         
+    <div className="shadow">
+      
+      <img src={image} alt={label} />
+          </div>
+
+         <div className="food-title">{label}</div>
+
+      
+  
+      <button onClick={() => setShow(!show)}>Ingredients</button>
+      {show && <RecipeDetails ingredients={ingredients} />}
+                <a href={url} target="_blank" rel="noopener noreferrer">
+        view full recipe
+      </a>
+
+    </div>
+
+  );
+};
+
+const RecipeDetails = ({ ingredients }) => {
+  return ingredients.map(ingredient => {
+    return (
+      <ul key={uuidv4()} className="ingredient-list">
+        <li className="ingredient-text">{ingredient.text}</li>
+        <li className="ingredient-weight">Weight - {ingredient.weight}</li>
+      </ul>
+    );
+  });
+};
+
 function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const APP_ID = "4e9f05eb";
+  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = e => setQuery(e.target.value);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    getData();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe App</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] &&
+          recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe} />)}
+      </div>
     </div>
   );
 }
